9d6cc6d95ae735ee7bcc957ae28e2d0e18a9ff8a
Adam Coffman <coffman.adam@gmail.com>
2011-01-18 21:10:02 -0600

    message passing working. client count broadcast

--- feednode.js
+++ feednode.js
@@ -15,4 +15,27 @@ app.get('/', function(req, res){
 	res.render('index');	
 });
 
+var activeClients = 0;
+
+function clientDisconnect(client){
+  activeClients -=1;
+  client.broadcast({clients:activeClients})
+}
+
+function sendNewItems(){
+  socket.broadcast({item:"<li>test item</li>", category:"test category"}); 
+}
+
+socket.on('connection', function(client){ 
+  activeClients +=1;
+  socket.broadcast({clients:activeClients})
+  client.on('disconnect', function(){clientDisconnect(client)});
+}); 
+
 app.listen(3000)
+
+//update messages on a timer - this method is preferrable to setInterval for our purposes
+setTimeout(function(){
+  sendNewItems();
+  setTimeout(arguments.callee, 1000);
+}, 1000);
--- public/feed.js
+++ public/feed.js
@@ -1,12 +1,6 @@
 var MAX_ITEMS = 5;
 var ITEMS = 0;
 
-function handleNewFeedItems(items){
-  for(item in items){
-
-  }
-}
-
 function fadeInOneItem(item, list){
   //add to top of list as invisible
   var $inserted = $(item).css({
@@ -14,28 +8,33 @@ function fadeInOneItem(item, list){
       opacity : 0,
       marginBottom: 0
   }).prependTo(list);
+  ITEMS += 1;
+
+  //remove the last item if we're at our max
+  if(ITEMS > MAX_ITEMS){
+    $list.children(':last').fadeOut(function(){$(this).remove()});
+    ITEMS -=1;
+  }
 
   //animate in the new item
   $inserted.slideDown({queue:false, duration:500})
     .animate( {marginBottom:'5px'}, {queue:false, duration:500} )
     .animate( {opacity:1}, {queue:false, duration:500} );
+}
 
-  ITEMS += 1;
-
-  if(ITEMS > MAX_ITEMS){
-    $list.children(':last').fadeOut(function(){$(this).remove()});
-    ITEMS -=1;
-  }
+function msgReceived(msg){
+  if(msg.clients)
+    $clientCounter.html(msg.clients);
+  else if(msg.item)
+    fadeInOneItem(msg.item,$list);
 }
 
 $(document).ready(function () {
   $list = $("#feed_list");
   $categories = $("#category_list");
+  $clientCounter = $("#client_count")
 
   var socket = new io.Socket(null, {port: 3000});
   socket.connect();
-  socket.on('newfeeditems', function(msg){
-    alert(msg);
-  });
-
+  socket.on('message', function(msg){msgReceived(msg)});
 });
