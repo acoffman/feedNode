f02cf9c3daf87825d28f036d8bd9e8dbb4f07c9d
Adam Coffman <coffman.adam@gmail.com>
2011-01-21 22:34:32 -0600

    sample ruby client

---
+++ sample_client.rb
@@ -0,0 +1,12 @@
+%w(curb uri).each{|lib| require lib}
+
+class FeedNodeClient
+  def initialize(server,app_token ,port=nil)
+    @target = "http://#{server}/#{app_token}/"
+  end
+
+  def send_msg(msg)
+    res = Curl::Easy.http_post(URI.escape(@target + msg))
+    throw Exception.new("There was a problem - make sure your token is correct") unless res.response_code == 200
+  end
+end
